d2d41484df84f3d825a774f1b10df4c35747ada8
Elena Williams <git@elena.net.au>
2018-07-17 17:48:55 +1000

    Reorganise library urls

--- project/library/urls.py
+++ project/library/urls.py
@@ -1,22 +1,29 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
-from . import views
 from django.conf.urls import include, url
+from django.contrib.auth.decorators import login_required
+
+from . import views
 
 
 urls = [
 
-    url(r'^$', views.GameListView.as_view(), name='category_list'),
 
-    url(r'^(?P<slug>[\w-]+)/$', views.GameListView.as_view(),
+    url(r'^categories/$',
+        login_required(views.CategoryListView.as_view()),
+        name='category_list'),
+
+    url(r'^c/(?P<slug>[\w-]+)/$',
+        login_required(views.GameListView.as_view()),
         name='category_detail'),
 
-    url(r'^(?P<slug>[\w-]+)/$', views.GameListView.as_view(),
-        name='game_list'),
-
-    url(r'^game/(?P<slug>[\w-]+)/$', views.GameDetailView.as_view(),
+    url(r'^g/(?P<slug>[\w-]+)/$',
+        login_required(views.GameDetailView.as_view()),
         name='game_detail'),
 
+    url(r'^all/$',
+        login_required(views.GameListView.as_view()),
+        name='category_list'),
 ]
 
 
