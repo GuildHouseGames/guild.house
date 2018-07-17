4a764d6da3f17a725ef382fe5a038405b92b10ee
Elena Williams <git@elena.net.au>
2018-07-18 01:27:07 +1000

    Fix merge conflicts

--- project/library/admin.py
+++ project/library/admin.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 from .forms import CategoryAdminForm, GameAdminForm
-from .models import Category, Game
+from .models import Category, Game, GameInLibrary, GameRelated
 from django.contrib import admin
 
 
@@ -9,17 +9,17 @@ from django.contrib import admin
 class CategoryAdmin(admin.ModelAdmin):
 
     fieldsets = [
-        (None, {'fields': ['name']}),
-        ('Content', {'fields': ['title', 'heading', 'featured_content',
-                                'content', 'meta_description']}),
-        ('Publishing', {'fields': ['is_enabled', 'is_featured', 'site', 'slug',
-                                   ('created_at', 'updated_at')],
+        (None, {'fields': ['name', 'slug', 'featured_content',
+                           'content', 'meta_description']}),
+        ('Publishing', {'fields': ['title', 'heading',
+                                   'is_enabled', 'is_featured',
+                                   'site', ('created_at', 'updated_at')],
                         'classes': ['collapse']}),
     ]
 
     form = CategoryAdminForm
 
-    list_display = ['name', 'is_active', 'is_featured']
+    list_display = ['name', 'get_url', 'is_active', 'is_featured']
 
     list_filter = ['is_enabled', 'is_featured']
 
@@ -28,6 +28,31 @@ class CategoryAdmin(admin.ModelAdmin):
     readonly_fields = ['created_at', 'updated_at']
 
     search_fields = ['name']
+
+    def get_url(self, obj):
+        return "<a target='_blank'  href={}>{}</a>".format(
+            obj.get_absolute_url(),
+            obj.slug
+        )
+    get_url.allow_tags = True
+
+
+@admin.register(GameInLibrary)
+class GameInLibraryAdmin(admin.ModelAdmin):
+
+    model = GameInLibrary
+
+
+class GameInLibraryInline(admin.TabularInline):
+
+    model = GameInLibrary
+
+
+class GameRelatedInline(admin.TabularInline):
+
+    model = GameRelated
+
+    fk_name = 'game'
 
 
 @admin.register(Game)
@@ -35,28 +60,66 @@ class GameAdmin(admin.ModelAdmin):
 
     fieldsets = [
         (None, {'fields': [
-            'name', 'categories', 'expansion_for',
-            'publisher', 'boardgamegeek_id', 'boardgamegeek_rank',
-            'boardgamegeek_img', 'year_published',
+            'name', 'categories', 'expansion_for', 'publisher',
             ('minimum_players', 'maximum_players'),
             ('minimum_playtime', 'maximum_playtime'),
+            # 'related',
         ]}),
-        ('Content', {'fields': ['title', 'heading', 'featured_content',
-                                'featured_image', 'content',
-                                'meta_description']}),
-        ('Publishing', {'fields': ['is_enabled', 'is_featured', 'site', 'slug',
-                                   'tags', ('created_at', 'updated_at')],
-                        'classes': ['collapse']}),
+        ('Scraped', {
+            'fields': [
+                'boardgamegeek_id', 'boardgamegeek_rank',
+                'boardgamegeek_img', 'publisher',
+                'year_published',
+            ],
+            'classes': ['collapse']
+        }),
+        ('Content', {'fields': [
+            'title', 'heading', 'featured_content',
+            'featured_image', 'content',
+            'meta_description'
+        ]}),
+        ('Publishing', {
+            'fields': [
+                'is_enabled', 'is_featured', 'site', 'slug',
+                'tags', ('created_at', 'updated_at')
+            ],
+            'classes': ['collapse']}
+         ),
     ]
 
     form = GameAdminForm
 
-    list_display = ['name', 'is_active', 'is_featured', 'is_expansion']
+    list_display = [
+        'name',
+        'is_new',
+        'is_featured',
+        'display_categories',
+        'display_related',
+        'display_copies',
+    ]
 
-    list_filter = ['is_enabled', 'is_featured']
+    list_filter = ['is_enabled', 'is_featured', 'categories']
+
+    list_editable = ['is_new', 'is_featured']
 
     prepopulated_fields = {'slug': ['name']}
 
     readonly_fields = ['created_at', 'updated_at']
 
     search_fields = ['name']
+
+    inlines = [GameInLibraryInline, GameRelatedInline]
+
+    def display_categories(self, obj):
+        return ", ".join([x.name for x in obj.categories.all()])
+    display_categories.short_description = 'Categories'
+
+    def display_related(self, obj):
+        return ", ".join([x.related.name for x in obj.gamerelated_set.all()])
+    display_related.short_description = 'Related'
+
+    def display_copies(self, obj):
+        return "{} <a href='/admin/library/gameinlibrary/add/'>+</a>".format(
+            GameInLibrary.objects.filter(game=obj).count())
+    display_copies.short_description = 'Live Copies'
+    display_copies.allow_tags = True
